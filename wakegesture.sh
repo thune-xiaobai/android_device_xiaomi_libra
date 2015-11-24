e25c64bf3c42d6736a2aaee924d24536375bb0c6
crancocco <fengz181x@126.com>
2015-11-24 20:15:10 +0800

    update dt2w

--- wakegesture.sh
+++ wakegesture.sh
@@ -2,6 +2,12 @@
 
 #doubletap to wake
 export WAKEUP_GESTURE_FILE=`ls /sys/devices/soc.0/f9924000.i2c/i2c-2/2-0070/input/input?/wake_gesture | head -n 1`
-chown system system $WAKEUP_GESTURE_FILE
-ln -sf $WAKEUP_GESTURE_FILE /data/wake_gesture
-chown system system /data/wake_gesture
+
+if [ -f /data/wake_gesture ];then
+    echo "ok"
+else
+    rm /data/wake_gesture
+    chown system system $WAKEUP_GESTURE_FILE
+    ln -sf $WAKEUP_GESTURE_FILE /data/wake_gesture
+    chown system system /data/wake_gesture
+fi
