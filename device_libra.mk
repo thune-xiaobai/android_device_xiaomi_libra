04286a83897e09e87466c86a38083458c8897301
crancocco <fengz181x@126.com>
2015-12-11 15:38:20 +0800

    update gps.conf

--- device_libra.mk
+++ device_libra.mk
@@ -1,8 +1,5 @@
 $(call inherit-product, $(SRC_TARGET_DIR)/product/languages_full.mk)
 
-# The gps config appropriate for this device
-$(call inherit-product, device/common/gps/gps_us_supl.mk)
-
 $(call inherit-product-if-exists, vendor/xiaomi/libra/libra-vendor.mk)
 
 DEVICE_PACKAGE_OVERLAYS += device/xiaomi/libra/overlay
