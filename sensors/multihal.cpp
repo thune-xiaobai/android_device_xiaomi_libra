27cd7a31c9f15894b0a13f0e8f1d1abc8d88bd59
Ketut Putu Kumajaya <ketut.kumajaya@gmail.com>
2016-04-15 22:20:28 +0700

    libra: Modify multi HAL sensor to properly load LSM6DB0
    Change-Id: I06852a4dc74add3cca0f9fda6c3a63cda5797633

--- sensors/multihal.cpp
+++ sensors/multihal.cpp
@@ -441,7 +441,9 @@ static int device__poll(struct sensors_poll_device_t *dev, sensors_event_t* data
 static int device__batch(struct sensors_poll_device_1 *dev, int handle,
         int flags, int64_t period_ns, int64_t timeout) {
     sensors_poll_context_t* ctx = (sensors_poll_context_t*) dev;
-    return ctx->batch(handle, flags, period_ns, timeout);
+    (void)(flags);
+    (void)(timeout);
+    return ctx->setDelay(handle, period_ns);
 }
 
 static int device__flush(struct sensors_poll_device_1 *dev, int handle) {
@@ -612,7 +614,7 @@ struct sensors_module_t HAL_MODULE_INFO_SYM = {
     common :{
         tag : HARDWARE_MODULE_TAG,
         version_major : 1,
-        version_minor : 1,
+        version_minor : 0,
         id : SENSORS_HARDWARE_MODULE_ID,
         name : "MultiHal Sensor Module",
         author : "Google, Inc",
@@ -633,7 +635,7 @@ static int open_sensors(const struct hw_module_t* hw_module, const char* name,
     sensors_poll_context_t *dev = new sensors_poll_context_t();
     memset(dev, 0, sizeof(sensors_poll_device_1_t));
     dev->proxy_device.common.tag = HARDWARE_DEVICE_TAG;
-    dev->proxy_device.common.version = SENSORS_DEVICE_API_VERSION_1_3;
+    dev->proxy_device.common.version = SENSORS_DEVICE_API_VERSION_1_0;
     dev->proxy_device.common.module = const_cast<hw_module_t*>(hw_module);
     dev->proxy_device.common.close = device__close;
     dev->proxy_device.activate = device__activate;
