dea8ddce354000b16848eb41780ede02c5fb7adc
xiaobai <fengz181x@126.com>
2017-03-29 00:46:49 +0800

    libra: small clean up

--- rootdir/Android.mk
+++ rootdir/Android.mk
@@ -20,14 +20,6 @@ include $(CLEAR_VARS)
 # files that live under device/qcom/common/rootdir/etc/
 
 include $(CLEAR_VARS)
-LOCAL_MODULE       := init.class_main.sh
-LOCAL_MODULE_TAGS  := optional eng
-LOCAL_MODULE_CLASS := ETC
-LOCAL_SRC_FILES    := etc/init.class_main.sh
-LOCAL_MODULE_PATH  := $(TARGET_ROOT_OUT)
-include $(BUILD_PREBUILT)
-
-include $(CLEAR_VARS)
 LOCAL_MODULE       := init.mdm.sh
 LOCAL_MODULE_TAGS  := optional eng
 LOCAL_MODULE_CLASS := ETC
