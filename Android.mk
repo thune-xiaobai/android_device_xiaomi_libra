fda141eb6830a96608d5a2341c525226bcb43ffc
xiaobai <fengz181x@126.com>
2017-04-04 16:48:05 +0800

    libra:remove unused and create ims symlink
    Change-Id: Id6b1dd6b72899b2577ed3adf3dc8c43439aacba9

--- Android.mk
+++ Android.mk
@@ -20,4 +20,25 @@ ifeq ($(TARGET_DEVICE),libra)
 
 include $(call all-makefiles-under,$(LOCAL_PATH))
 
+# WiFi symlinks
+$(shell mkdir -p $(TARGET_OUT_ETC)/firmware/wlan/qca_cld; \
+    ln -sf /system/etc/wifi/WCNSS_qcom_cfg.ini \
+	    $(TARGET_OUT_ETC)/firmware/wlan/qca_cld/WCNSS_qcom_cfg.ini; \
+    ln -sf /persist/wlan_mac.bin \
+	    $(TARGET_OUT_ETC)/firmware/wlan/qca_cld/wlan_mac.bin)
+# END WiFi symlinks
+
+# IMS lib symlink
+IMS_LIBS := libimscamera_jni.so libimsmedia_jni.so
+
+IMS_SYMLINKS := $(addprefix $(TARGET_OUT_VENDOR_APPS)/ims/lib/arm64/,$(notdir $(IMS_LIBS)))
+$(IMS_SYMLINKS): $(LOCAL_INSTALLED_MODULE)
+	@echo "IMS lib link: $@"
+	@mkdir -p $(dir $@)
+	@rm -rf $@
+	$(hide) ln -sf /system/vendor/lib64/$(notdir $@) $@
+
+ALL_DEFAULT_INSTALLED_MODULES += $(IMS_SYMLINKS)
+# END IMS lib symlink
+
 endif
