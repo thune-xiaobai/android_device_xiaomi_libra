579d35a6e67fef0627f24bfb167c0243247c55cb
xiaobai <fengz181x@126.com>
2017-04-05 00:11:00 +0800

    libra: add missing makefile includes

--- mokee.mk
+++ mokee.mk
@@ -1,6 +1,10 @@
 # Release name
 PRODUCT_RELEASE_NAME := libra
 
+# Inherit from those products. Most specific first.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+
 # Inherit some common CM stuff.
 $(call inherit-product, vendor/mk/config/common_full_phone.mk)
 
