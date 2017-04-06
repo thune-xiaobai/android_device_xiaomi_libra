a919806bbf3169b93e13949e7a22f6f23e50e539
martincz <martincz@mokeedev.com>
2017-04-06 22:47:42 +0800

    libra: Drep
    Change-Id: Ic01914febc5292e20511c9b9d28d742425cc050f

--- extract-files.sh
+++ extract-files.sh
@@ -24,9 +24,9 @@ VENDOR=xiaomi
 MY_DIR="${BASH_SOURCE%/*}"
 if [[ ! -d "$MY_DIR" ]]; then MY_DIR="$PWD"; fi
 
-CM_ROOT="$MY_DIR"/../../..
+MK_ROOT="$MY_DIR"/../../..
 
-HELPER="$CM_ROOT"/vendor/cm/build/tools/extract_utils.sh
+HELPER="$MK_ROOT"/vendor/mk/build/tools/extract_utils.sh
 if [ ! -f "$HELPER" ]; then
     echo "Unable to find helper script at $HELPER"
     exit 1
@@ -53,7 +53,7 @@ else
 fi
 
 # Initialize the helper
-setup_vendor "$DEVICE" "$VENDOR" "$CM_ROOT"
+setup_vendor "$DEVICE" "$VENDOR" "$MK_ROOT"
 
 extract "$MY_DIR"/proprietary-files.txt "$SRC"
 if [ -n "$RADIO_SRC" ]; then
