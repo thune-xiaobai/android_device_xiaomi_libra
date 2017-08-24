2a60fc0f262d1dc653a3c5a306887e702671f8f5
OzzysCmAcc <oskarkratochvil86@yahoo.com>
2017-08-24 22:05:10 +0200

    libra: add HIDL manifest
    Change-Id: I3848e3c9163647080dc653fbd54eabe6e9ec1e35

--- device.mk
+++ device.mk
@@ -174,6 +174,10 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     android.hardware.gnss@1.0-impl
 
+# HIDL
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/manifest.xml:system/vendor/manifest.xml
+
 # Doze
 PRODUCT_PACKAGES += \
     LibraDoze
