14b290dcdb2ad05256f7b70267f4d1854c3cc600
xiaobai <fengz181x@126.com>
2017-09-05 17:01:26 +0800

    libra: fix a mistake

--- device.mk
+++ device.mk
@@ -137,10 +137,10 @@ PRODUCT_PACKAGES += \
 
 # Display
 PRODUCT_PACKAGES += \
-    copybit.msm8994 \
-    gralloc.msm8994 \
-    hwcomposer.msm8994 \
-    memtrack.msm8994 \
+    copybit.msm8992 \
+    gralloc.msm8992 \
+    hwcomposer.msm8992 \
+    memtrack.msm8992 \
     liboverlay \
     libtinyxml \
     android.hardware.graphics.allocator@2.0-impl \
